28e579db9453ef10b13eb71825c8e2b7df75593b
Jan Schupke <jan@schupke.io>
2025-12-02 07:43:38 +0000

    Full code check

--- packages/i18n/src/i18n.ts
+++ packages/i18n/src/i18n.ts
@@ -6,28 +6,26 @@ import adminEn from './locales/en/admin.json';
 import apiEn from './locales/en/api.json';
 
 export const initI18n = () => {
-  i18n
-    .use(initReactI18next)
-    .init({
-      resources: {
-        en: {
-          common: commonEn,
-          client: clientEn,
-          admin: adminEn,
-          api: apiEn,
-        },
+  i18n.use(initReactI18next).init({
+    resources: {
+      en: {
+        common: commonEn,
+        client: clientEn,
+        admin: adminEn,
+        api: apiEn,
       },
-      lng: 'en',
-      fallbackLng: 'en',
-      defaultNS: 'common',
-      ns: ['common', 'client', 'admin', 'api'],
-      interpolation: {
-        escapeValue: false, // React already escapes values
-      },
-      react: {
-        useSuspense: false,
-      },
-    });
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    defaultNS: 'common',
+    ns: ['common', 'client', 'admin', 'api'],
+    interpolation: {
+      escapeValue: false, // React already escapes values
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
 
   return i18n;
 };
@@ -35,4 +33,3 @@ export const initI18n = () => {
 // Initialize i18n
 export { i18n };
 initI18n();
-
