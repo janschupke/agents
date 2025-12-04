9d949caf5049e037c7cc1c70fb454690bb20d129
Jan Schupke <jan@schupke.io>
2025-12-04 11:18:41 +0000

    Config fixed

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
   outDir: 'dist',
+  external: ['react', 'react-dom', 'react-router-dom'],
 });
